6143fb9228f627f4869d1ba22d3cf95a5b362197
jareddingman <jareddingman@gmail.com>
2025-10-02 19:50:32 -0500

    1

---
+++ assignment2.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
+
+data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
+
+Y = data['meal']
+# make sure you drop a column with the axis=1 argument
+columns_to_drop = ['id', 'meal', 'DateTime']
+X = data.drop(columns=columns_to_drop)
+
+
+x, xt, y, yt = train_test_split(X, Y, test_size=0.2)
+
+model = XGBClassifier(n_estimators=50, max_depth=10, learning_rate=0.4, objective='binary:logistic')
+
+y = y.to_frame()
+
+modelFit = model.fit(x, y)
+
+pred = model.predict(xt)
+
+print(accuracy_score(yt, pred)*100)
