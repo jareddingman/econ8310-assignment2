15ecd22a6d63272be297f1b99d4e49bb36637eb6
jareddingman <jareddingman@gmail.com>
2025-10-02 20:17:13 -0500

    4

--- assignment2.py
+++ assignment2.py
@@ -8,13 +8,14 @@ data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/Assignmen
 
 Y = data['meal']
 # make sure you drop a column with the axis=1 argument
-columns_to_drop = ['id', 'meal', 'DateTime']
-X = data.drop(columns=columns_to_drop)
+columns_to_drop = ['id', 'DateTime', 'meal']
+X = data.drop(columns=columns_to_drop, axis=1)
 
 
-x, xt, y, yt = train_test_split(X, Y, test_size=0.2)
 
-model = XGBClassifier(n_estimators=50, max_depth=10, learning_rate=0.4, objective='binary:hinge')
+x, xt, y, yt = train_test_split(X, Y, test_size=0.07051) #the test size is to get 1000
+
+model = XGBClassifier(n_estimators=100, max_depth=4, learning_rate=0.4, objective='binary:logistic')
 
 y = y.to_frame()
 
@@ -22,5 +23,7 @@ modelFit = model.fit(x, y)
 
 pred1 = model.predict(xt)
 pred = pred1.flatten().tolist()
+#not workng rn bc length is not 1000
 
 print(accuracy_score(yt, pred)*100)
+
