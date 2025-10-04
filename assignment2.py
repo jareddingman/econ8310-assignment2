65e070a7b43fa87569f4f2b2627ebce99ec2b653
jareddingman <jareddingman@gmail.com>
2025-10-04 15:19:16 -0500

    11

--- assignment2.py
+++ assignment2.py
@@ -6,7 +6,6 @@ from xgboost import XGBClassifier
 
 data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3.csv")
 
-
 Y = data['meal']
 # make sure you drop a column with the axis=1 argument
 columns_to_drop = ['id', 'DateTime', 'meal']
@@ -14,22 +13,28 @@ X = data.drop(columns=columns_to_drop, axis=1)
 
 
 
-x, xt, y, yt = train_test_split(X, Y, test_size=1000) 
+x, xt, y, yt = train_test_split(X, Y, test_size=1000, stratify=Y, random_state=67) 
 
-#0/ len(data)
 
-model = XGBClassifier(n_estimators=2000, max_depth=8, learning_rate=0.1, subsample=.9, colsample_bytree=.8, objective='binary:logistic', eval_metric = 'logloss')
+model = XGBClassifier(n_estimators=2000, 
+                      max_depth=12, 
+                      learning_rate=0.4,
+                      gamma=1, #this punishes super complex models
+                      min_child_weight=5,
+                      objective='binary:logistic', 
+                      eval_metric = 'logloss')
 
 y = y.to_frame()
 
 modelFit = model.fit(x, y)
 
-pred = model.predict_proba(xt)[:, 1].astype(float).tolist()
+
+pred1 = modelFit.predict(xt)
+pred = pred1.flatten().tolist()
 
 
 
-# pred1 = modelFit.predict(xt)
-# pred = pred1.flatten().tolist()
+print(accuracy_score(yt, pred)*100)
 
 
 
