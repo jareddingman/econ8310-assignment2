8a947229d0ef98fe0498ad4b994cd9b59869f37a
jareddingman <jareddingman@gmail.com>
2025-10-06 12:11:39 -0500

    1

--- assignment2.py
+++ assignment2.py
@@ -16,17 +16,11 @@ x, xt, y, yt = train_test_split(X, Y, test_size=1000, random_state=67)
 
 from sklearn.ensemble import RandomForestClassifier
 
-# Generate the random forest model
 model = RandomForestClassifier(n_estimators=1500, 
                                max_depth=25, 
                                n_jobs = -1,
                               min_samples_leaf=2,
                               min_samples_split=4,
                             	random_state=67)
-# Fit the model to the training data
 modelFit = model.fit(x, y)
-# Make predictions
 pred = model.predict(xt)
-# Print the accuracy score of the fitted model
-print("The random forest has an accuracy of : %s\n" 
-	% str(accuracy_score(pred, yt)))
